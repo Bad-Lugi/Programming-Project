cf3f873b19a6b53e81afb4c41619ab4b1ec08a3a
Tyler <Weresquirrel@icloud.com>
2021-04-05 10:06:47 -0600

    Color Changer

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    Material mat;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Color")
+        {
+            mat = this.GetComponent<MeshRenderer>().material;
+            this.GetComponent<MeshRenderer>().material = other.GetComponent<MeshRenderer>().material;
+            other.GetComponent<MeshRenderer>().material = mat;
+        }
+
+    }
+}
