068ffd0604c68815744036251d7e9370d5fc82d3
Tyler <Weresquirrel@icloud.com>
2021-04-07 10:09:51 -0600

    Karnes told me too

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class ColorChanger : MonoBehaviour
 {
     Material mat;
+    public Material p1Mat;
+    private void Awake()
+    {
+        if(GameObject.FindGameObjectsWithTag("Player").Length <= 1)
+        {
+            this.GetComponent<MeshRenderer>().material = p1Mat;
 
+            GameObject.FindGameObjectWithTag("MainCamera").SetActive(false);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Color")
