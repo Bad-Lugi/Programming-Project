c91397d895483e5c2b421b34ed39cd7a0d80c1c9
Tyler <Weresquirrel@icloud.com>
2021-04-09 10:37:31 -0600

    For Me Don't Touch

---
+++ Assets/Scripts/StartingLine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingLine : MonoBehaviour
+{
+    public StartArea starter;
+    public bool playerOn;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        playerOn = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.name);
+        playerOn = false;
+    }
+}
