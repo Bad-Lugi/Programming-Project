12e38e3fbfabbfc00622e52a49fa227d01d8daac
Tyler <Weresquirrel@icloud.com>
2021-04-09 10:20:08 -0600

    Better Color Stuff

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -6,11 +6,14 @@ public class ColorChanger : MonoBehaviour
 {
     Material mat;
     public Material p1Mat;
+    public Material p1SMat;
+    public Material Solid;
     private void Awake()
     {
         if(GameObject.FindGameObjectsWithTag("Player").Length <= 1)
         {
             this.GetComponent<MeshRenderer>().material = p1Mat;
+            Solid = p1SMat;
 
             GameObject.FindGameObjectWithTag("MainCamera").SetActive(false);
         }
@@ -21,6 +24,12 @@ public class ColorChanger : MonoBehaviour
         {
             mat = this.GetComponent<MeshRenderer>().material;
             this.GetComponent<MeshRenderer>().material = other.GetComponent<MeshRenderer>().material;
+            other.GetComponent<MeshRenderer>().material = mat;
+        }
+        if (other.tag == "SColor")
+        {
+            mat = Solid;
+            Solid = other.GetComponent<MeshRenderer>().material;
             other.GetComponent<MeshRenderer>().material = mat;
         }
 
