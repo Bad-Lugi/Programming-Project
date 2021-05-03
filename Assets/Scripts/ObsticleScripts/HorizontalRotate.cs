f1a181610575f8bdf2ee329dae8f216277068607
Maddox <maddoxwilton@gmail.com>
2021-05-03 09:22:09 -0600

    Fan room
    need to check if the death works

---
+++ Assets/Scripts/ObsticleScripts/HorizontalRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalRotate : MonoBehaviour
+{
+
+    public float rotateSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotate();  
+    }
+
+    void rotate()
+    {
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.World);
+    }
+}
