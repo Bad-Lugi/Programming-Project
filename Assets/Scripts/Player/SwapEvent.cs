1d48377136c36925cfc7d2db8f0626ca166db360
Tyler <Weresquirrel@icloud.com>
2021-05-11 10:52:31 -0600

    Spin

---
+++ Assets/Scripts/Player/SwapEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapEvent : MonoBehaviour
+{
+    public Animation anim;
+    public void Show()
+    {
+        anim.Play();
+    }
+    public void Hide()
+    {
+
+    }
+
+}
