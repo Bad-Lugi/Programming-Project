aa0c7a3dff42358fed11c2488a7a3afeab0eeee1
Tyler <Weresquirrel@icloud.com>
2021-05-13 09:28:14 -0600

    Im Done

--- Assets/Scripts/Player/SwapEvent.cs
+++ Assets/Scripts/Player/SwapEvent.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class SwapEvent : MonoBehaviour
 {
-    public Animation anim;
-    public void Show()
-    {
-        anim.Play();
-    }
+    public GameObject arrows;
+    public GameObject player;
+    public GameObject OtherPlayer;
     public void Hide()
     {
-
+        
+        arrows.SetActive(false);
     }
+    public void Swap()
+    {
+        Vector3 thisplayer = this.gameObject.transform.position;
+        player.transform.position = new Vector3(-OtherPlayer.gameObject.transform.position.x, this.gameObject.transform.position.y, OtherPlayer.gameObject.transform.position.z);
+        this.gameObject.GetComponent<JellyMesh>().spawn(this.transform);
+        OtherPlayer.gameObject.transform.position = new Vector3(-thisplayer.x, this.gameObject.transform.position.y, thisplayer.z);
+        OtherPlayer.gameObject.GetComponent<JellyMesh>().spawn(OtherPlayer.transform);
+    }
+
 
 }
